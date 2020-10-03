03455755951da0202376c8a393217090287d2aa3
Darken141 <darken141@gmail.com>
2020-10-04 01:00:30 +0200

    fix drawer, prototype map section

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -157,6 +157,12 @@ module.exports = {
     }
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-react-leaflet',
+      options: {
+        linkStyles: true // (default: true) Enable/disable loading stylesheets via CDN
+      }
+    },
     `gatsby-plugin-scroll-reveal`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
--- client/src/sections/mapSection/mapSection.jsx
+++ client/src/sections/mapSection/mapSection.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const MapSection = () => {
-    return (
-        <section>
-            <h1>Map section</h1>
-        </section>
-    )
+    const position = [51.505, -0.09]
+
+    if (typeof window !== 'undefined') {
+        return (
+            <section style={{ width: "100%", height: "60vh" }}>
+                <Map style={{ height: "100%" }} center={position} zoom={13}>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    />
+                    <Marker position={position}>
+                        <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+                    </Marker>
+                </Map>
+            </section>
+        )
+    }
+    return null
 }
 
 export default MapSection
