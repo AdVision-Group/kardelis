36059360f1a747ee0695880616931b2dcc7d7a06
Darken141 <darken141@gmail.com>
2020-10-03 17:22:04 +0200

    create useMenuItems hook

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,20 +1,26 @@
 import React from "react"
+import { useMenuItems } from '../../hooks/useMenuItems'
 
-const Header = () => (
-  <header>
-    <div>
-      <h1>Logo</h1>
-    </div>
-    <nav>
-      <ul>
-        <li>
-          <a href='/'>Link</a>
-        </li>
-        <li>
-          <a href='/'>Link</a>
-        </li>
-      </ul>
-    </nav>
-  </header>
-)
+const Header = () => {
+  const [navLinks] = useMenuItems()
+
+  return (
+    <header>
+      <div>
+        <h1>Logo</h1>
+      </div>
+      <nav>
+        <ul>
+          {
+            navLinks.map(({ name, slug }, idx) => (
+              <li key={idx}>
+                <a href={slug}>{name}</a>
+              </li>
+            ))
+          }
+        </ul>
+      </nav>
+    </header>
+  )
+}
 export default Header
