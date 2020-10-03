c4ad9b9080a16a5b1948963d9dc5e03c4ff5320e
Darken141 <darken141@gmail.com>
2020-10-03 23:25:34 +0200

    prototype services section

--- client/src/sections/servicesSection/servicesSection.jsx
+++ client/src/sections/servicesSection/servicesSection.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import Image from 'gatsby-image'
 
-const ServicesSection = () => {
+import { servicesStyles, containerStyles, rowStyles, colStyles, illuStyles, outlineStyles } from './servicesSection.module.scss'
+
+const ServicesSection = ({ data }) => {
+    const query = useStaticQuery(graphql`
+    {
+      allFile(filter: {relativeDirectory: {eq: "services"}}) {
+        nodes {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+    `)
+
+    console.log(query.allFile.nodes[0].childImageSharp.fluid)
+
     return (
-        <section>
-            <h1>Services section</h1>
+        <section className={servicesStyles}>
+            {data.map(({ heading, content }, idx) => {
+                return (
+                    <div className={containerStyles} key={idx}>
+                        <div className='container'>
+                            {idx === 0 && <div className={illuStyles} />}
+                            <div className={rowStyles}>
+                                <div className={colStyles}>
+                                    <Image fluid={query.allFile.nodes[idx].childImageSharp.fluid} />
+                                    <div className={outlineStyles} />
+                                </div>
+                                <div className={colStyles}>
+                                    <h2>{heading}</h2>
+                                    <p>{content}</p>
+                                </div>
+                            </div>
+                            {idx === 1 && <div className={illuStyles} />}
+                        </div>
+                    </div>
+                )
+            })}
         </section>
     )
 }
