c468371ee0649b33935c04b343e622d9887c3113
Darken141 <darken141@gmail.com>
2020-10-03 21:20:22 +0200

    fix warning message about clickable elements

--- client/src/components/hamMenu/hamMenu.jsx
+++ client/src/components/hamMenu/hamMenu.jsx
@@ -5,11 +5,11 @@ import { openHamStyles, closeHamStyles } from './hamMenu.module.scss'
 
 const HamMenu = ({ isOpen = false, handleClick = null }) => {
     return (
-        <div onClick={handleClick} className={isOpen ? openHamStyles : closeHamStyles}>
+        <button onClick={handleClick} className={isOpen ? openHamStyles : closeHamStyles}>
             <div />
             <div />
             <div />
-        </div>
+        </button>
     )
 }
 
