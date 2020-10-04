cb68f1892d52fb5c6cd6c7143c85303e5c01a453
Darken141 <darken141@gmail.com>
2020-10-05 00:54:42 +0200

    mapSection: add map coordinates

--- client/src/sections/mapSection/mapSection.jsx
+++ client/src/sections/mapSection/mapSection.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const MapSection = () => {
-    const position = [51.505, -0.09]
+    const position = [50.044060, 14.516790]
 
     if (typeof window !== 'undefined') {
         return (
             <section style={{ width: "100%", height: "60vh" }}>
-                <Map style={{ height: "100%" }} center={position} zoom={13}>
+                <Map style={{ height: "100%" }} center={position} zoom={18}>
                     <TileLayer
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
