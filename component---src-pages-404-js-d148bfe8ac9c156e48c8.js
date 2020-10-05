803945299fb84d529e666655d06d9cd73af27c68
Darken141 <darken141@gmail.com>
2020-10-05 02:17:17 +0200

    Updates

---
+++ component---src-pages-404-js-d148bfe8ac9c156e48c8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("JMja"),r=n("vrFN");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(r.a,{title:"404: Stránka sa nenašla"}),l.a.createElement("h1",null,"404: Not Found"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-d148bfe8ac9c156e48c8.js.map
