ba0fdde72e364db829abe8a4911470940f9d24b4
Darken141 <darken141@gmail.com>
2020-10-14 16:25:30 +0200

    change logo, servicesSection: rebuild section, minor adjusments'

--- client/src/sections/servicesSection/servicesSection.jsx
+++ client/src/sections/servicesSection/servicesSection.jsx
@@ -1,55 +1,75 @@
-import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
+import React, { useState } from 'react'
 import Image from 'gatsby-image'
 
-import { servicesStyles, containerStyles, rowStyles, colStyles, illuStyles, outlineStyles } from './servicesSection.module.scss'
+import {
+  servicesStyles,
+  illuStyles,
+  headingStyles,
+  illuStylesBottom,
+  serviceImgStyles,
+  serviceStyles,
+  servicesContainerStyles,
+  descriptionStyles,
+  descStyles,
+  descStylesActive,
+  serviceImgStylesActive
+} from './servicesSection.module.scss'
 
-const ServicesSection = ({ data, wave }) => {
-  const query = useStaticQuery(graphql`
+const ServicesSection = ({ wave }) => {
+  const services = [
     {
-      allFile(filter: {relativeDirectory: {eq: "services"}}) {
-        nodes {
-          childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid_withWebp
-            }
-          }
-        }
-      }
+      title: "Lorem ipsum",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet nulla facilisi morbi tempus. Elit ut aliquam purus sit amet luctus venenatis. Pellentesque habitant morbi tristique senectus et. Augue ut lectus arcu bibendum at varius. Leo a diam"
+    },
+    {
+      title: "Lorem ipsum",
+      desc: "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini."
+    },
+    {
+      title: "Lorem ipsum",
+      desc: "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip. Sea lettuce lettuce water chestnut eggplant winter purslane fennel azuki bean earthnut pea sierra leone bologi leek soko chicory celtuce parsley jï¿½cama salsify."
     }
-    `)
+  ]
+
+  const [activeCard, setActiveCard] = useState(1)
 
   return (
     <section id='sluzby' className={servicesStyles}>
-      {data.map(({ heading, content }, idx) => {
-        return (
-          <div className={containerStyles} key={idx}>
-            <div className='container'>
-              {idx === 0 && (<div className={illuStyles}>
-                <Image fluid={wave} />
-              </div>)}
-              <div
-                className={rowStyles}
-                data-sal="slide-up"
-                data-sal-duration="1500"
-                data-sal-easing="ease"
-              >
-                <div className={colStyles}>
-                  <Image fluid={query.allFile.nodes[idx].childImageSharp.fluid} />
-                  <div className={outlineStyles} />
-                </div>
-                <div className={colStyles}>
-                  <h2>{heading}</h2>
-                  <p>{content}</p>
-                </div>
-              </div>
-              {idx === 1 && (<div className={illuStyles} >
-                <Image fluid={wave} />
-              </div>)}
-            </div>
+      <div className='container'>
+        <div className={headingStyles}>
+          <h2>Naše služby</h2>
+          <div className={illuStyles}>
+            <Image fluid={wave} />
           </div>
-        )
-      })}
+        </div>
+
+        <div className={servicesContainerStyles}>
+
+          {services.map(({ title }, idx) => {
+            return (
+              <div key={idx} className={serviceStyles}>
+                <div className={activeCard === idx ? serviceImgStylesActive : serviceImgStyles} onClick={() => setActiveCard(idx)} />
+                <h3>{title}</h3>
+              </div>
+            )
+          })}
+        </div>
+
+        <div className={descriptionStyles}>
+          {services.map(({ desc }, idx) => {
+            return (
+              <p key={idx} className={activeCard === idx ? descStylesActive : descStyles}>
+                {desc}
+              </p>
+            )
+          })}
+        </div>
+
+        <div className={illuStylesBottom}>
+          <Image fluid={wave} />
+        </div>
+      </div>
+
     </section>
   )
 }
